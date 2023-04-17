d7566d53298ad35b6b0c81b09fedc396784cfb57
alirezamou <am808036@gmail.com>
2023-04-17 16:43:12 +0330

    add product state, mutation and action in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,26 @@
 import { createStore } from "vuex";
 
-const store = createStore({});
+import { getProducts } from "../services/productServices";
+
+const store = createStore({
+  state: {
+    products: [],
+  },
+  mutations: {
+    SET_PRODUCTS(state, products) {
+      state.products = products;
+    },
+  },
+  actions: {
+    async getProducts({ commit }) {
+      try {
+        const products = await getProducts();
+        commit("SET_PRODUCTS", products);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+});
 
 export default store;
