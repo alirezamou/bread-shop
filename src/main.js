05d83255c225065fed8c71cca7ff5c02e62e286c
alirezamou <am808036@gmail.com>
2023-04-18 17:48:38 +0330

    move fontawesome imports and initilisation into separate module

--- src/main.js
+++ src/main.js
@@ -5,22 +5,12 @@ import store from "./store";
 
 import "bootstrap/dist/css/bootstrap.rtl.min.css";
 import "bootstrap";
-
 import "./assets/main.css";
-
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import {
-  faIcons,
-  faPlus,
-  faArrowLeftLong,
-} from "@fortawesome/free-solid-svg-icons";
-
-library.add(faIcons, faPlus, faArrowLeftLong);
 
 const app = createApp(App);
 
-app.component("fa-icon", FontAwesomeIcon);
+import fontawesomePlugin from "./plugins/packages/fontawesome";
+fontawesomePlugin(app);
 
 app.use(router);
 
